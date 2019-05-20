88fbdbeee2bc9c326929c056f5ce8106f0ea73f9
ATCKUUOHAHNCT <leon.kowyrshin@yandex.ru>
2019-05-20 11:00:00 +0700

    Update main

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include "board.h"
+#include "board_print_plain.h"
+#include <stdio.h>
+
+char init_desk[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                        {'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'}};
+
+char desk[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                   {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                   {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                   {'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'}};
+
+int main()
+{
+    char p[5];
+    int b[5];
+    int s = 0;
+
+    clear();
+    printf("Zdravstvuite\n\nWhite (big)\n\n");
+
+    while (1) {
+        print_desk();
+        input(p, b);
+        clear();
+
+        while (!check_input(b, p) || !check_figure_move(b) || !Player(b, s)) {
+            printf("Error..\n\n");
+            print_desk();
+            printf("\n");
+            input(p, b);
+            clear();
+        }
+
+        move(b);
+        s++;
+    }
+}
